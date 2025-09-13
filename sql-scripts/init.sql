87956f5cad108746d52a1ece07833ddfcef72fd0
Henrique Custodio <h.custodio.dev@gmail.com>
2025-09-13 14:13:46 -0400

    Add: init.sql

---
+++ sql-scripts/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS hostedftp
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+USE hostedftp;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username   VARCHAR(50)  NOT NULL UNIQUE,
+  password   VARCHAR(255) NOT NULL
+);
+
