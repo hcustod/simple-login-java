cdb8f7b364f72817d35963ead5ee90f879ea92f9
Henrique Custodio <h.custodio.dev@gmail.com>
2025-09-20 17:08:16 -0400

    Update: init.sql matching db updates

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -6,8 +6,9 @@ USE hostedftp;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username   VARCHAR(50)  NOT NULL UNIQUE,
-  password   VARCHAR(255) NOT NULL
-);
+  id            INT AUTO_INCREMENT PRIMARY KEY,
+  username      VARCHAR(100) NOT NULL UNIQUE,            
+  password_hash TEXT         NOT NULL,                  
+  created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
